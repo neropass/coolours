c37f92468dcac17a31a03d9e624886af96be57ca
Nero Argento <drakonobor@gmail.com>
2023-10-31 23:26:48 +0300

    changed color randomizer to chroma one

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const cols = document.querySelectorAll('.col');
 
-function generateRandomColor() {
+/* function generateRandomColor() {
   const hexCodes = '0123456789ABCDEF';
   let color = '';
   for (let i = 0; i < 6; i++) {
     color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
   }
   return '#' + color;
-}
+} */
 
 function setRandomColors() {
   cols.forEach(col => {
     const text = col.querySelector('h2');
-    const color = generateRandomColor()
+    const color = chroma.random();
 
     text.textContent = color;
     col.style.background = color;
