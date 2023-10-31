f5ad9c1e4e361f7d8f157b7afb824edec14d1e82
Nero Argento <drakonobor@gmail.com>
2023-11-01 00:03:27 +0300

    added colors hashing for sharing

--- app.js
+++ app.js
@@ -36,25 +36,37 @@ function copyToClipboard(text) {
 }
 
 function setRandomColors() {
+  const colors = [];
   cols.forEach(col => {
     const isLocked = col.querySelector('i').classList.contains('fa-lock');
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
     const color = chroma.random();
 
-    if (isLocked) return;
+    if (isLocked) {
+      colors.push(text.textContent);
+      return;
+    }
     
+    colors.push(color);
+
     text.textContent = color;
     col.style.background = color;
 
     setTextColor(text, color);
     setTextColor(button, color);
   })
+
+  updateColorsHash(colors);
 }
 
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+function updateColorsHash(colors = []) {
+  document.location.hash = colors.toString();
 }
 
 setRandomColors();
